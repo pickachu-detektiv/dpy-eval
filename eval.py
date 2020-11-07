3c822244c4b3be02c1c860af3d1b2d0f2686e37c
toxic-recker <62395124+toxicrecker@users.noreply.github.com>
2020-11-07 18:29:38 +0530

    Update eval.py

--- eval.py
+++ eval.py
@@ -1,7 +1,22 @@
 @bot.command(name="eval")
 @commands.is_owner()
 async def eval_fn(ctx, *, code):
-    code.strip("` ")
+    language_specifiers = ["python", "py", "javascript", "js", "html", "css", "php", "md", "markdown", "go", "golang", "c", "c++", "cpp", "c#", "cs", "csharp", "java", "ruby", "rb", "coffee-script", "coffeescript", "coffee", "bash", "shell", "sh", "json", "http", "pascal", "perl", "rust", "sql", "swift", "vim", "xml", "yaml"]
+    loops = 0
+    while code.startswith("`"):
+        code = "".join(list(code)[1:])
+        loops += 1
+        if loops == 3:
+            loops = 0
+            break
+    for language_specifier in language_specifiers:
+        if code.startswith(language_specifier):
+            code = code.lstrip(language_specifier)
+    while code.endswith("`"):
+        code = "".join(list(code)[0:-1])
+        loops += 1
+        if loops == 3:
+            break
     code = "\n".join(f"    {i}" for i in code.splitlines()) #Adds an extra layer of indentation
     code = f"async def eval_expr():\n{code}" #Wraps the code inside an async function
     def send(text): #Function for sending message to discord if code has any usage of print function
